4b6768128e41b3df0ddc24934a381cb88fe25390
Tamilselvan K <tamilselvank761@gmail.com>
2025-04-29 11:47:11 +0530

    Time: 7 ms (13.33%), Space: 8.8 MB (82.22%) - LeetHub

--- 3176-minimum-sum-of-mountain-triplets-i/3176-minimum-sum-of-mountain-triplets-i.c
+++ 3176-minimum-sum-of-mountain-triplets-i/3176-minimum-sum-of-mountain-triplets-i.c
@@ -1,15 +1,13 @@
 int minimumSum(int* nums, int n) {
     int i, j, k, sum, minSum = INT_MAX;
-    for(i = 0; i < n - 2; i++){
-        for(j = i + 1; j < n - 1; j++){
-            for(k = j + 1; k < n; k++){
-                if(nums[i] < nums[j] && nums[k] < nums[j]){
-                    sum = nums[i] + nums[j] + nums[k];
-                    if(sum < minSum)
-                        minSum = sum; 
-                }
-            }
-        }
-    }
+    for(i = 0; i < n - 2; i++)
+        for(j = i + 1; j < n - 1; j++)
+            if(nums[i] < nums[j])
+                for(k = j + 1; k < n; k++)
+                    if(nums[k] < nums[j]){
+                        sum = nums[i] + nums[j] + nums[k];
+                        if(sum < minSum)
+                            minSum = sum; 
+                    }
     return (minSum == INT_MAX) ? -1 : minSum;
 }
