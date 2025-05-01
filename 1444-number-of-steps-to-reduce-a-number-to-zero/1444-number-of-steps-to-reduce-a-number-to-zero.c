e7718628c200b995216c565495282d68b2b91e46
Tamilselvan K <tamilselvank761@gmail.com>
2025-05-01 10:45:24 +0530

    Time: 0 ms (100%), Space: 7.9 MB (47.23%) - LeetHub

---
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.c
@@ -0,0 +1,11 @@
+int numberOfSteps(int num) {
+    int c = 0;
+    while(num != 0){
+        c++;
+        if(num % 2 == 0)
+            num /= 2;
+        else 
+            num -= 1;
+    }
+    return c;
+}
