10981a6c4f536fc31267031fb09e323cba46e1a7
Tamilselvan K <tamilselvank761@gmail.com>
2025-05-27 09:25:50 +0530

    Time: 2 ms (9.79%), Space: 11.1 MB (35.35%) - LeetHub

--- 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.c
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.c
@@ -6,19 +6,20 @@
  * };
  */
 struct ListNode* deleteDuplicates(struct ListNode* head) {
-    if(head == NULL)
-        return NULL;
-    struct ListNode *temp = head, *prev = head;
-    temp = temp -> next;
-    while(temp != NULL){
-        if(temp -> val == prev -> val){
-            prev -> next = temp -> next;
-            struct ListNode *dupNode = temp;
-            temp = temp -> next;
+    // if(head == NULL)
+    //     return NULL;
+    struct ListNode *temp = head;
+    // struct ListNode *prev = head;
+    // temp = temp -> next;
+    while(temp != NULL && temp -> next != NULL){
+        if(temp -> val == temp -> next -> val){
+            // prev -> next = temp -> next;
+            struct ListNode *dupNode = temp -> next;
+            temp -> next = temp -> next -> next;
             free(dupNode);
         }
         else{
-            prev = temp;
+            // prev = temp;
             temp = temp -> next;
         } 
     }
